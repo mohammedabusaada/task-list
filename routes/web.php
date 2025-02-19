c12aed676e56f65aed8d4415ac02e509fb962c2b
mohammedabusaada <mommdabused@gmail.com>
2025-02-19 09:22:47 -0800

    After dealing with Database

--- routes/web.php
+++ routes/web.php
@@ -28,3 +28,36 @@ Route::post('/about', function(){
     ];
     return view('about', compact('name', 'departments'));
 });
+
+
+Route::get('tasks', function(){
+    $tasks = DB::table('tasks')->get();
+    return view('tasks', compact('tasks'));
+});
+
+
+Route::post('create', function(){
+    $task_name = $_POST['name'];
+    DB::table('tasks')->insert(['name' => $task_name]);
+    return redirect()->back();
+});
+
+
+Route::post('delete/{id}', function($id){
+    DB::table('tasks')->where('id', $id)->delete();
+    return redirect()->back();
+});
+
+
+Route::post('edit/{id}', function($id){
+    $task = DB::table('tasks')->where('id', $id)->first();
+    $tasks = DB::table('tasks')->get();
+    return view('tasks', compact('task', 'tasks'));
+});
+
+
+Route::post('update', function(){
+    $id = $_POST['id'];
+    DB::table('tasks')->where('id', $id)->update(['name' => $_POST['name']]);
+    return redirect('tasks');
+});
